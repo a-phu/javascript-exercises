a32051a4c1874645499c18c3f63100ee826e75c6
a-phu <annabel.phu@hotmail.com>
2021-12-21 22:29:30 +1100

    can pass first two tests using for loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, element, element2) {
+    let index = 0;
+    let index2 = 0;
+    for(let i = 0; i < array.length; i++){
+        if(element === array[i]){
+            index = i;
+        } 
+        if(element2 !== undefined){
+            if(element2 === array[i]){
+                index2 = i;
+            }
+        }
+   }
+   //splice returns the value removed and removes it
+    array.splice(index, 1);
+    if(element2 !== undefined){
+        array.splice(index2, 1);
+    }
+    return array;
 };
 
+let array = [1, 2, 3, 4];
+console.log(removeFromArray(array, 3, 2));
 // Do not edit below this line
-module.exports = removeFromArray;
+// module.exports = removeFromArray;
