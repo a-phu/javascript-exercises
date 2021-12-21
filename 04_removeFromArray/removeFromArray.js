ab0a86cec5f7f99ad240c22397c4766d48600a23
a-phu <annabel.phu@hotmail.com>
2021-12-22 07:48:45 +1100

    passed 4 tests - adds everything that doesn't match parameters to new array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,25 +1,15 @@
 const removeFromArray = function(array, element, element2) {
-    let index = 0;
-    let index2 = 0;
+    let remainingArray = [];
     for(let i = 0; i < array.length; i++){
-        if(element === array[i]){
-            index = i;
-        } 
-        if(element2 !== undefined){
-            if(element2 === array[i]){
-                index2 = i;
-            }
-        }
-   }
-   //splice returns the value removed and removes it
-    array.splice(index, 1);
-    if(element2 !== undefined){
-        array.splice(index2, 1);
+       if(array[i] !== element && array[i] !== element2){
+           remainingArray.push(array[i]);
+       }
     }
-    return array;
+   
+    return remainingArray;
 };
 
-let array = [1, 2, 3, 4];
-console.log(removeFromArray(array, 3, 2));
+// let array = [1, 2, 3, 4];
+// console.log(removeFromArray(array, 3, 7));
 // Do not edit below this line
-// module.exports = removeFromArray;
+module.exports = removeFromArray;
