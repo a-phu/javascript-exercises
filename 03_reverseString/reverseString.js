d57040d512964e268f13bba22f31d92ef84360cf
a-phu <annabel.phu@hotmail.com>
2021-12-22 22:25:18 +1100

    passed all tests using while loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
 const reverseString = function(str) {
+    const split = str.split('');
+    let i = split.length - 1;
+    let counter = 0;
+    let reversed = '';
 
+    while(counter < split.length){
+        reversed += split[i];
+        i--;
+        counter++;
+    }
+    
+    return reversed;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
