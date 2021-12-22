d027c8293b7562e39ff18c31d01919c29e85b39d
a-phu <annabel.phu@hotmail.com>
2021-12-22 22:14:03 +1100

    passed all tests - added elements that didn't match any parameters passed to a new array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,29 @@
-const removeFromArray = function(array, element, element2) {
-    let remainingArray = [];
-    for(let i = 0; i < array.length; i++){
-       if(array[i] !== element && array[i] !== element2){
-           remainingArray.push(array[i]);
-       }
-    }
+const removeFromArray = function(array) {
+   let args = Array.prototype.slice.call(arguments, 1);
+   let match = '';
+   let newArray = [];
    
-    return remainingArray;
+   for(let i = 0; i < array.length; i++){
+       //check to see if array element matches anything in arguments passed
+       for(let j = 0; j < args.length; j++){
+           //if match found, assign that to variable match
+           if(array[i] === args[j]) match = array[i];
+       }
+
+       //if match != '', then not matched to parameters passed and not removed
+       if(match === ''){
+           newArray.push(array[i]);
+       }
+
+       //reset checker
+       match = '';
+   }
+
+   return newArray;
+   
 };
 
 // let array = [1, 2, 3, 4];
-// console.log(removeFromArray(array, 3, 7));
+// console.log(removeFromArray(array, 3, 7, 'tacos', 1));
 // Do not edit below this line
 module.exports = removeFromArray;
