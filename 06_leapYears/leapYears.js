f8cd074ff7e10b547c1825e071121d69e99e5c88
a-phu <annabel.phu@hotmail.com>
2021-12-23 13:36:19 +1100

    passed all tests using && operators

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //if year not divisible by 4 with no remainders
+    if(year % 4 !== 0){
+        return false;
+    } 
 
+    //if year divisible by 100 but not divisible by 400
+    if(year % 100 === 0 && year % 400 !== 0){
+        return false;
+    }
+
+    return true;
 };
 
+console.log(leapYears(1985));
 // Do not edit below this line
 module.exports = leapYears;
