5bba71b9509adec273b5e1fcb130caef6f07e76a
a-phu <annabel.phu@hotmail.com>
2021-12-23 13:26:32 +1100

    passed all tests using array sort function and array some function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,15 @@
-const sumAll = function(start, end) {
-    let sum = 0;
+const sumAll = function(...args) {
+    let listToSum = args.sort((a, b) => a - b);
+    if(listToSum.some(item => item < 0) || listToSum.some(item => typeof item !== "number")) return 'ERROR';
 
-    for(let i = start; i <= end; i++){
+    let sum = 0;
+    for(let i = listToSum[0]; i <= listToSum[listToSum.length-1]; i++){
         sum += i;
     }
 
     return sum;
 };
 
-// console.log(sumAll(1, 4));
+console.log(sumAll('yo', 4).toString());
 // Do not edit below this line
 module.exports = sumAll;
