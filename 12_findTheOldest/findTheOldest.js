8674653f8a9953e153d79b90c27add1c698a6436
a-phu <annabel.phu@hotmail.com>
2022-01-03 14:10:41 +1100

    odin solution with notes

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,14 +1,34 @@
-const findTheOldest = function(peopleArray) {
-    return peopleArray.reduce((oldest, current) => {
-        if(!oldest.hasOwnProperty('yearOfDeath')) oldest['yearOfDeath'] = new Date().getFullYear();
-        if(!current.hasOwnProperty('yearOfDeath')) current['yearOfDeath'] = new Date().getFullYear();
-        let oldestYearsLived = oldest.yearOfDeath - oldest.yearOfBirth;
-        console.log("Oldest " + oldest.name + oldestYearsLived);
-        let currentYearsLived = current.yearOfDeath - current.yearOfBirth;
-        console.log("Current " + current.name + currentYearsLived);
-        return oldestYearsLived > currentYearsLived ? oldest : current;
+// const findTheOldest = function(peopleArray) {
+//     return peopleArray.reduce((oldest, current) => {
+//         if(!oldest.hasOwnProperty('yearOfDeath')) oldest['yearOfDeath'] = new Date().getFullYear();
+//         if(!current.hasOwnProperty('yearOfDeath')) current['yearOfDeath'] = new Date().getFullYear();
+//         let oldestYearsLived = oldest.yearOfDeath - oldest.yearOfBirth;
+//         console.log("Oldest " + oldest.name + oldestYearsLived);
+//         let currentYearsLived = current.yearOfDeath - current.yearOfBirth;
+//         console.log("Current " + current.name + currentYearsLived);
+//         return oldestYearsLived > currentYearsLived ? oldest : current;
+//     });
+// };
+
+//odin solution
+const findTheOldest = function(peopleArray){
+    return peopleArray.reduce((oldest, current) =>{
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return oldestAge < currentAge ? current : oldest;
     });
-};
+}
+
+
+const getAge = function(birth, death){
+    //if object has no 'yearOfDeath' property, get current year
+    if(!death){
+        death = new Date().getFullYear();
+
+    }
+
+    return death - birth;
+}
 
 const people = [
     {
