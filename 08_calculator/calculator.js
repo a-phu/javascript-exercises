43ad3831c8217745a4d5d8ffeb552634abc368cd
a-phu <annabel.phu@hotmail.com>
2022-01-03 13:26:53 +1100

    passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = array => parseInt(array.reduce((accelerator, current) => accelerator + current, 0));
+
+const multiply = function(array) {
+  return parseInt(array.reduce((accelerator, current) => accelerator * current, 1));
 };
 
-const multiply = function() {
+const power = (base, exponent) => Math.pow(base, exponent);
 
+const factorial = function(a) {
+  if(a === 0) return 1;
+  let total = 1;
+  for(let i = a; i >= 1; i--){
+    total *= i;
+  }
+  return total;
 };
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+  
 
 // Do not edit below this line
 module.exports = {
